3bf35d17c5234ab9d0a3dbde3a775fc35cb0a686
Zhang Huixin <rzxyxxzhx1103@gmail.com>
2023-04-25 18:02:23 -0500

    fix

--- docs/js/items.js
+++ docs/js/items.js
@@ -39,39 +39,15 @@ $(document).ready(function () {
             "type": "1"
         },
         {
-            "name": "Dresser - Dark Wood",
+            "name": "Shelf",
             "image": "models/thumbnails/thumbnail_matera_dresser_5.png",
             "model": "models/js/DWR_MATERA_DRESSER2.js",
             "type": "1"
         },
         {
-            "name": "Dresser - White",
+            "name": "Storage",
             "image": "models/thumbnails/thumbnail_img25o.jpg",
             "model": "models/js/we-narrow6white_baked.js",
-            "type": "1"
-        },
-        {
-            "name": "Bedside table - Shale",
-            "image": "models/thumbnails/thumbnail_Blu-Dot-Shale-Bedside-Table.jpg",
-            "model": "models/js/bd-shalebedside-smoke_baked.js",
-            "type": "1"
-        },
-        {
-            "name": "Bedside table - White",
-            "image": "models/thumbnails/thumbnail_arch-white-oval-nightstand.jpg",
-            "model": "models/js/cb-archnight-white_baked.js",
-            "type": "1"
-        },
-        {
-            "name": "Wardrobe - White",
-            "image": "models/thumbnails/thumbnail_TN-ikea-kvikine.png",
-            "model": "models/js/ik-kivine_baked.js",
-            "type": "1"
-        },
-        {
-            "name": "Full Bed",
-            "image": "models/thumbnails/thumbnail_nordli-bed-frame__0159270_PE315708_S4.JPG",
-            "model": "models/js/ik_nordli_full.js",
             "type": "1"
         },
         {
@@ -81,25 +57,13 @@ $(document).ready(function () {
             "type": "1"
         },
         {
-            "name": "Media Console - White",
+            "name": "Media Console",
             "image": "models/thumbnails/thumbnail_clapboard-white-60-media-console-1.jpg",
             "model": "models/js/cb-clapboard_baked.js",
             "type": "1"
         },
         {
-            "name": "Media Console - Black",
-            "image": "models/thumbnails/thumbnail_moore-60-media-console-1.jpg",
-            "model": "models/js/cb-moore_baked.js",
-            "type": "1"
-        },
-        {
-            "name": "Sectional - Olive",
-            "image": "models/thumbnails/thumbnail_img21o.jpg",
-            "model": "models/js/we-crosby2piece-greenbaked.js",
-            "type": "1"
-        },
-        {
-            "name": "Sofa - Grey",
+            "name": "Sofa",
             "image": "models/thumbnails/thumbnail_rochelle-sofa-3.jpg",
             "model": "models/js/cb-rochelle-gray_baked.js",
             "type": "1"
@@ -117,7 +81,7 @@ $(document).ready(function () {
             "type": "1"
         },
         {
-            "name": "Coffee Table - Wood",
+            "name": "Coffee Table",
             "image": "models/thumbnails/thumbnail_stockholm-coffee-table__0181245_PE332924_S4.JPG",
             "model": "models/js/ik-stockholmcoffee-brown.js",
             "type": "1"
@@ -129,13 +93,13 @@ $(document).ready(function () {
             "type": "1"
         },
         {
-            "name": "Dining Table",
+            "name": "Round Table",
             "image": "models/thumbnails/thumbnail_scholar-dining-table.jpg",
             "model": "models/js/cb-scholartable_baked.js",
             "type": "1"
         },
         {
-            "name": "Dining table",
+            "name": "Meeting table",
             "image": "models/thumbnails/thumbnail_Screen_Shot_2014-01-28_at_6.49.33_PM.png",
             "model": "models/js/BlakeAvenuejoshuatreecheftable.js",
             "type": "1"
