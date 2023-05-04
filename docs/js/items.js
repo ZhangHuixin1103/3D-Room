e3166fd8cde8ea7097f601f2a3906e26a31d2a83
Zhang Huixin <rzxyxxzhx1103@gmail.com>
2023-05-03 22:29:50 -0500

    rename

--- docs/js/items.js
+++ docs/js/items.js
@@ -4,109 +4,109 @@ $(document).ready(function () {
     var items = [
         {
             "name": "Closed Door",
-            "image": "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
+            "image": "models/thumbnails/door_closed.png",
             "model": "models/js/closed-door28x80_baked.js",
             "type": "7"
         },
         {
             "name": "Open Door",
-            "image": "models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.22.46_PM.png",
+            "image": "models/thumbnails/door_open.png",
             "model": "models/js/open_door.js",
             "type": "7"
         },
         {
             "name": "Window",
-            "image": "models/thumbnails/thumbnail_window.png",
+            "image": "models/thumbnails/window.png",
             "model": "models/js/whitewindow.js",
             "type": "3"
         },
         {
             "name": "Chair",
-            "image": "models/thumbnails/thumbnail_Church-Chair-oak-white_1024x1024.jpg",
+            "image": "models/thumbnails/chair.jpg",
             "model": "models/js/gus-churchchair-whiteoak.js",
             "type": "1"
         },
         {
             "name": "Red Chair",
-            "image": "models/thumbnails/thumbnail_tn-orange.png",
+            "image": "models/thumbnails/chair_red.png",
             "model": "models/js/ik-ekero-orange_baked.js",
             "type": "1"
         },
         {
             "name": "Blue Chair",
-            "image": "models/thumbnails/thumbnail_ekero-blue3.png",
+            "image": "models/thumbnails/chair_blue.png",
             "model": "models/js/ik-ekero-blue_baked.js",
             "type": "1"
         },
         {
             "name": "Shelf",
-            "image": "models/thumbnails/thumbnail_matera_dresser_5.png",
+            "image": "models/thumbnails/shelf.png",
             "model": "models/js/DWR_MATERA_DRESSER2.js",
             "type": "1"
         },
         {
             "name": "Storage",
-            "image": "models/thumbnails/thumbnail_img25o.jpg",
+            "image": "models/thumbnails/storage.jpg",
             "model": "models/js/we-narrow6white_baked.js",
             "type": "1"
         },
         {
             "name": "Bookshelf",
-            "image": "models/thumbnails/thumbnail_kendall-walnut-bookcase.jpg",
+            "image": "models/thumbnails/bookcase.jpg",
             "model": "models/js/cb-kendallbookcasewalnut_baked.js",
             "type": "1"
         },
         {
             "name": "Media Console",
-            "image": "models/thumbnails/thumbnail_clapboard-white-60-media-console-1.jpg",
+            "image": "models/thumbnails/media.jpg",
             "model": "models/js/cb-clapboard_baked.js",
             "type": "1"
         },
         {
             "name": "Sofa",
-            "image": "models/thumbnails/thumbnail_rochelle-sofa-3.jpg",
+            "image": "models/thumbnails/sofa.jpg",
             "model": "models/js/cb-rochelle-gray_baked.js",
             "type": "1"
         },
         {
             "name": "Wooden Trunk",
-            "image": "models/thumbnails/thumbnail_teca-storage-trunk.jpg",
+            "image": "models/thumbnails/trunk.jpg",
             "model": "models/js/cb-tecs_baked.js",
             "type": "1"
         },
         {
             "name": "Floor Lamp",
-            "image": "models/thumbnails/thumbnail_ore-white.png",
+            "image": "models/thumbnails/lamp.png",
             "model": "models/js/ore-3legged-white_baked.js",
             "type": "1"
         },
         {
             "name": "Coffee Table",
-            "image": "models/thumbnails/thumbnail_stockholm-coffee-table__0181245_PE332924_S4.JPG",
+            "image": "models/thumbnails/table_coffee.JPG",
             "model": "models/js/ik-stockholmcoffee-brown.js",
             "type": "1"
         },
         {
             "name": "Side Table",
-            "image": "models/thumbnails/thumbnail_Screen_Shot_2014-02-21_at_1.24.58_PM.png",
+            "image": "models/thumbnails/table_side.png",
             "model": "models/js/GUSossingtonendtable.js",
             "type": "1"
         },
         {
             "name": "Round Table",
-            "image": "models/thumbnails/thumbnail_scholar-dining-table.jpg",
+            "image": "models/thumbnails/table_rnd.jpg",
             "model": "models/js/cb-scholartable_baked.js",
             "type": "1"
         },
         {
             "name": "Meeting Table",
-            "image": "models/thumbnails/thumbnail_Screen_Shot_2014-01-28_at_6.49.33_PM.png",
+            "image": "models/thumbnails/table_rec.png",
             "model": "models/js/BlakeAvenuejoshuatreecheftable.js",
             "type": "1"
         },
         {
             "name": "Poster",
-            "image": "models/thumbnails/thumbnail_nyc2.jpg",
+            "image": "models/thumbnails/poster.jpg",
             "model": "models/js/nyc-poster2.js",
             "type": "2"
         },
@@ -155,7 +155,7 @@ $(document).ready(function () {
         */
         {
             "name": "Rug",
-            "image": "models/thumbnails/thumbnail_cb-blue-block60x96.png",
+            "image": "models/thumbnails/rug.png",
             "model": "models/js/cb-blue-block-60x96.js",
             "type": "8"
         }
